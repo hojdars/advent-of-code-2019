6290df9d13eaf721520dad9471deb651d4579cc8
hojdars <hojdar.stepan@gmail.com>
2019-12-06 20:08:40 +0100

    Task 5, Part 2, JMPs, EQ, LT

--- 5/sol.py
+++ 5/sol.py
@@ -1,5 +1,5 @@
 # instruction : number of params
-INSTRUCTIONS = {1 : 3, 2 : 3, 3 : 1, 4 : 1, 99 : 0}
+INSTRUCTIONS = {1 : 3, 2 : 3, 3 : 1, 4 : 1, 5 : 2, 6 : 2, 7 : 3, 8 : 3, 99 : 0}
 
 def get_opcode(intcode):
     """ Convert intcode in memory into opcode.
@@ -49,7 +49,7 @@ def execute_instruction(opcode, modes, params, program):
         return program[params[i]] if modes[len(modes) - 1 - i] == 0 else params[i] 
 
     if opcode == 99: # HALT
-        return -1
+        return -1, None
     elif opcode == 1: # ADD
         op1 = eval_param(0)
         op2 = eval_param(1)
@@ -64,27 +64,49 @@ def execute_instruction(opcode, modes, params, program):
         assert params[0] < len(program)
         program[params[0]] = int(input())
     elif opcode == 4: # WRITE
-        assert params[0] < len(program)
         assert len(modes) == 1
         if modes[0] == 0:
+            assert params[0] < len(program)
             print(program[params[0]])
         elif modes[0] == 1:
             print(params[0])
         else:
             assert False
+    elif opcode == 5: # JMP if TRUE
+        check = eval_param(0)
+        ipset = eval_param(1)
+        if check != 0:
+            return -2, ipset
+    elif opcode == 6: # JMP if FALSE
+        check = eval_param(0)
+        ipset = eval_param(1)
+        if check == 0:
+            return -2, ipset
+    elif opcode == 7: # LESS THAN
+        op1 = eval_param(0)
+        op2 = eval_param(1)
+        dest = params[2]
+        program[dest] = 1 if op1 < op2 else 0
+    elif opcode == 8: # EQUALS
+        op1 = eval_param(0)
+        op2 = eval_param(1)
+        dest = params[2]
+        program[dest] = 1 if op1 == op2 else 0
     else:
         assert False
-        return -1
+        return -1, None
 
-    return INSTRUCTIONS[opcode] + 1
+    return INSTRUCTIONS[opcode] + 1, None
 
 def next_instruction(ip, program):
     opcode, modes = get_opcode(program[ip])
     params = program[ip+1:ip+INSTRUCTIONS[opcode]+1]
     assert len(params) == INSTRUCTIONS[opcode]
-    ip_shift = execute_instruction(opcode, modes, params, program)
+    ip_shift, ip_jmp = execute_instruction(opcode, modes, params, program)
     if ip_shift == -1:
         return -1
+    elif ip_shift == -2:
+        return ip_jmp
     else:
         return ip + ip_shift
 
@@ -116,10 +138,11 @@ def main():
 
 if __name__ == "__main__":
     run_prg("1002,4,3,4,33")
+    # run_prg("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99") # inpt EQ 8
     # run_prg("3,0,4,0,99") # manual test for INPUT and OUTPUT
 
-    # import doctest
-    # doctest.testmod()
-    print("--------------")
+    import doctest
+    doctest.testmod()
+
     main()
 
